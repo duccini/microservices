dfc91b2211c6c9ec35120d6f8ab9d6a49e31de4f
duccini <guilhermeduccini@gmail.com>
2024-07-28 13:29:38 -0300

    create query service

--- Blog/comments/index.js
+++ Blog/comments/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -15,7 +16,7 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[postId] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
 
   const postId = req.params.id;
@@ -28,7 +29,23 @@ app.post("/posts/:id/comments", (req, res) => {
 
   commentsByPostId[postId] = comments;
 
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id: commentId,
+      content,
+      postId,
+    },
+  });
+
   res.status(201).send(comments);
+});
+
+// Respond to Event Broker
+app.post("/events", (req, res) => {
+  console.log("Received Event", req.body.type);
+
+  res.send({});
 });
 
 app.listen(4001, () => {
--- Blog/posts/index.js
+++ Blog/posts/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -16,17 +17,34 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   // Service
   const id = randomBytes(4).toString("hex");
 
   const { title } = req.body;
+
   posts[id] = {
     id,
     title,
   };
 
+  // Emit an Event to Event Broker
+  await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
+    data: {
+      id,
+      title,
+    },
+  });
+
   res.status(201).send(posts[id]);
+});
+
+// Respond to Event Broker
+app.post("/events", (req, res) => {
+  console.log("Received Event", req.body.type);
+
+  res.send({});
 });
 
 app.listen(4000, () => {
